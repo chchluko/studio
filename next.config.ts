4be01d5c46627c211b189a5d908a6c98b48639b3
AzureAD\JesúsLópezEulogio <jesus.lopez@proa.com.mx>
2025-12-10 13:45:52 -0600

    se oculta boton

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  devIndicators: {
+    buildActivity: false, // Esto oculta el indicador de actividad de compilación
+  },
   images: {
     remotePatterns: [
       {
