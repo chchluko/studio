0563a95a1c04850a324c647f474d2885ac6d4109
AzureAD\JesúsLópezEulogio <jesus.lopez@proa.com.mx>
2025-12-11 16:52:20 -0600

    se reciben solicitudes desde distintas ips

---
+++ docs/inserts.sql
@@ -0,0 +1,137 @@
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('10153','Mauricio Romo Marín','Ingeniero Sr Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('10676','Iker Javier Egurbide Ayala','Ingeniero Jr Seguridad Plataformas Tec.',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('11274','Edgar Noé Estrada Flores','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('11342','Samantha Elizabeth Martínez Martínez','Jefe de Proyectos TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('11444','Héctor Manuel Vázquez Morales','Especialista Voz',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('11564','Manuel Magdiel Manriquez Trejo','Coordinador Proyectos Obras TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('11730','Jaime Peralta Rivero','Gerente Corporativo Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('12151','Eleazar Coeto Severiano','Ingeniero Soporte Foráneo Puebla',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('12228','Emilio Baruch Montoya Sánchez','Subdirector Infraestructura TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('13011','Marivel Zumaya Espinoza','Ingeniero Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('13307','Jorge Luis Flores Islas','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('13598','Fernando Jorge Toledo Yepez','Jefe Telecomunicaciones',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('14734','Mario Alberto Cano Granados','Jefe de Recursos TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('14777','José Axel Urquiza Vargas','Gerente Corporativo Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('14782','Manuel Alejandro Soto González','Jefe Corporativo SAP BASIS',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('14996','Cesar Eduardo Guizar Vilchis','Jefe Corporativo BD HANA SAP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15001','Luis Roberto Franco Ascencio','Jefe Corporativo SAP ABAP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15176','Daniel González Ortega','Jefe Corporativo Datos Maestros SAP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15223','Oscar Contreras Mondragón','Ingeniero Soporte Foráneo Michoacán',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15313','José Antonio Moro Morales','Jefe de Soporte Metro y Mesa de Ayuda',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('15753','Isaías Hernández Vázquez','Ingeniero Soporte Foráneo SLP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15754','Eduardo Daniel Gómez Rodríguez','Ingeniero Soporte Foráneo Jalisco',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15774','Jesús López Eulogio','Desarrollador PHP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15775','Abel Israel Olvera Ibarra','Ingeniero Soporte Foráneo Q. Roo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15777','Víctor Gregorio Cuellar Ovando','Supervisor Plataformas gabinete',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15778','Victor Manuel Serrano Cuevas','Ingeniero Soporte Foráneo Edo. Mex.',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('15837','Nestor Jesus Sanchez Gaspar','Jefe Soporte Foráneos Centro-Sur',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('16203','Eneas Espinosa Cerdio','Ingeniero Soporte Foráneo Chiapas',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('16337','Carlos Ivan Pérez Osorio','Ingeniero Soporte Foráneo Yucatán',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('16431','Montserrat Reyes Sánchez','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('16496','Abraham Gómez Hernández','Ingeniero Soporte Foráneo Chihuahua',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('16672','Jhonathan Valentin Cabrera Chávez','Ingeniero Soporte Foráneo Chihuahua',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('16911','Alexis Uriel Rangel Barrios','Ingeniero Soporte Foráneo Veracruz',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('17224','Edgar Armando Rosas Enríquez','Jefe Corporativo Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('17249','Gabriel Razo Carrera','Ingeniero Soporte Nocturno',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('17251','Víctor Manuel Zavala Morales','Jefe Corporativo SAP BASIS',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('17383','Eréndira María del Rocío Barajas Caballero','Analista Recursos TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('17426','Luis Orlando Campos Santos','Gerente Plataforma Tecnológicas',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('18087','Max Campos Esquivel','Ingeniero Sr Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('18416','Mario Armando Quiroz Navarro','Analista Dirección de TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('18738','Marcos Zendejas Sánchez','Ingeniero Soporte Primer Nivel',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('18797','Luis Enrique Sánchez Santiago','Gerente Corporativo Arquitectura Digital',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19087','Eunice Ortíz Sánchez','Jefe Corporativo Productos Tecnológicos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19156','Ricardo Ramírez Romero','Ingeniero Soporte Foráneo Pachuca',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19225','Jesús Alberto Castro Luna','Especialista de Productos Tecnológicos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19440','Marco Antonio Pérez López','Ingeniero Soporte Corporativo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19739','Josseline Judith Pichardo González','Analista Control de Activos TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19795','Esteban Galindo Franco','Ingeniero Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19825','Jose Antonio Juarez Castro','Especialista Herramientas Back Up',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19856','Lizbeth Diego Medina','Analista Admvo Ingeniería Biomédica',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('19869','Rhamses Rodríguez Hernández','Especialista Herramientas Control TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('19935','María Fabiola Canales Hernández','Especialista de Productos Tecnológicos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('20597','Javier Martínez Fernández','Subdirector Aplicaciones',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21024','Javier Antonio García Fernández','Especialista Cloud',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21377','Felipe de Jesús Galicia Santiago','Ingeniero Soporte Foráneo Guanajuato',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21379','David Cruz Manzo','Ingeniero Soporte Foráneo Aguascalientes',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21443','Juan Carlos Acosta Ríos','Ingeniero Soporte',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21493','Saul Martinez San Juan','Ingeniero Soporte Foráneo Tamaulipas',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21494','Eduardo Toriz Cisneros','Ingeniero Soporte Expansión',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21557','German Eduardo Aguilar Alvarado','Ingeniero Soporte Foráneo Mexicali',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('21653','Juan José Pacheco González','Coordinador Proyectos Obras TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('21835','José Paulo González Bustamante','Ingeniero Soporte Foráneo Tabasco',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('22319','José Luis Baena Jeronimo','Ingeniero Soporte Foráneo Monterrey',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('22493','Juan Antonio González Flores','Ingeniero Soporte Primer Nivel',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('22494','Mauricio Arturo Maldonado Guerra','Coordinador Proyectos Obras TI',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('22741','Josue Ivan Miranda Vargas','Ingeniero Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('22812','Moises Hernandez Murillo','Ingeniero Soporte Foráneo Monterrey',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23282','Guadalupe Selene González Bustamante','Ingeniero Soporte Remoto',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23288','Diana Aidee León Claudio','Ingeniero Jr Ing BM Mtto a Equipos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23449','Sergio Pablo Guzman Hernandez','Ingeniero Soporte Foráneo Comarca-Saltil',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('23517','Gustavo Rutilio Caballero Rios','Ingeniero Soporte Foráneo Oaxaca',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23717','Emma López Herrera','Consultor TI Análisis y Diseño',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23794','Irvin Uriel Aguilar Cosme','Consultor TI Análisis y Diseño',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23797','Abigail Romero Galindo','Consultor TI Análisis y Diseño',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23801','Jose Manuel Cruz Alonso','Ingeniero Soporte Foráneo Tijuana',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23861','Carolina Vieyra Machuca','Gerente Ingeniería Biomédica',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('23969','Rodolfo Quezada Huitron','Consultor TI Análisis y Diseño',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24190','Jenny Joana Rosas Celis','Jefe Corporativo ABAP SAP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24200','Axel Jimenez Cruz','Arquitecto Integración de Aplicaciones',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24334','Agapito Mata Ruiz','Ingeniero Soporte Foráneo Jalisco',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('24370','David Omar Perez Bautista','Jefe Corporativo SAP SD',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24380','Domingo Efrain Cuaquentzi Blanco','Ingeniero Soporte Foráneo Puebla',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24400','Guillermo Rincón Zavala','Ingeniero Sr Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24415','Victor Cesar Martínez Martínez','Ingeniero Soporte Expansión',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24425','Moisés Hernández López','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24433','Lissete Galarza Alvarado','Ingeniero Soporte Remoto',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24461','José Luis Hernández Álvarez','Ingeniero Soporte Foráneo Sinaloa',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24489','Luis Alberto Mendez Medina','Jefe Corporativo Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24508','Aldo David Estrada Martínez','Ingeniero Soporte Expansión',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24561','Alejandro Abenamar Chavarría Vallejos','Ingeniero Soporte Foráneo Campeche',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('24656','Francisco Alberto Rivera Paz','Ingeniero Soporte Foráneo Sonora',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('24956','Omar Mendoza Ordaz','Ingeniero Soporte Primer Nivel',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('26060','Jesús Fernando Leal Cruz','Ingeniero Jr Seguridad Telecomunicacion',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('2950','Miguel Ángel Arrona Sandoval','Ingeniero Sr Soporte Nivel 2',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('3235','Rodolfo Benjamín Díaz de León Tellez','Subdirector Sistemas Core',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('3380','Oscar González Campos','Subdirector de Plataformas de Datos y An',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('3962','Rafael Arturo Esparza Bautista','Gerente Soporte',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('4689','Fernando Orozco Pacheco','Líder de Proyecto Base de Datos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('4878','Gabriela Amaro Sarmiento','Ingeniero Jr Ing BM Mtto a Equipos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('4900','Álvaro Ramírez Ortíz','Ingeniero Jr Ing BM Mtto a Equipos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('5025','Mariana Zúñiga Sánchez','Jefe Aperturas',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('5085','Uzi Berenice Rosas Santillán','Líder de Proyecto Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('5179','Rosalba Morales López','Asistente de Dirección Digital',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('5595','Jose Luis Flores Santana','Ingeniero Soporte Remoto',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('6112','José de Jesús García Osorio','Líder de Proyecto Base de Datos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('7470','Ricardo Aguirre Lara','Especialista SAP',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('7607','Teófilo Melgar Guerrero','Ingeniero Jr Ing BM Mtto a Equipos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('7608','Hugo Zárate Hernández','Ingeniero Soporte Expansión',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('7716','Jesús Ulises Baltierra Medel','Ingeniero Jr Ing BM Mtto a Equipos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('7808','Héctor Hugo Ochoa Miguel','Desarrollador Sr. Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('8531','Guadalupe Susana Martínez Quiroz','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('8569','Beatriz García Rentería','Jefe Soporte ForÁneos Norte-Occidente',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9061','Mario Alberto Ruiz Montes de Oca','Especialista Sistemas Administrativos',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9354','Ariana García Díaz','Líder de Proyecto Desarrollo',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9355','Nayeli Jorge Robles','Desarrollador Sr. Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9368','José Armando Martínez Cosio','Desarrollador Sr. Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9373','Christian Alfonso Baca Téllez','Jefe Plataformas Gabinete',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9564','Ernesto Sam Mejía','Ingeniero Soporte Foráneo Querétaro',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('9876','Edith Moreno Jiménez','Jefe Corporativo SAP BW',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+INSERT INTO votacompa.colleagues (id,name,department,photoUrl,created_at,updated_at) VALUES
+	 ('4279','Oscar Adrián Valladares Contreras','Desarrollador Genexus',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04'),
+	 ('20890','Alejandro Azpe Olmedo','Subdirector Integración de Aplicaciones',NULL,'2025-12-10 10:05:04','2025-12-10 10:05:04');
+
+
+
